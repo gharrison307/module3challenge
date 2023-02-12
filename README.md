6d9b6f0b195610386e213cf002b3f6efbc45c9cf
George Harrison <g.a.harrison91@gmail.com>
2023-02-12 13:33:28 -0700

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# module3challenge
+# Password Generator Starter Code
+
+# <Module3challenge>
+
+## Module3challenge
+
+## Description
+
+[Project Screenshot](./assets/READMESCREENSHOT.JPG)
+
+This is the third module challenge of bootcamp. Using the starter code, I added additonal code to create a random password generator. First I defined the arrays that contained the various character options (lowercase, uppercase, numeric and special). The scprit then runs through a series of prompts and confirmations for the end user to define the password character criteria and produces a random passwrod from the selected character criteria.
+
+To use:
+
+To generate a password, first click the "Generate Password" button.
+
+The user will be promped to define the number of characters in the password. Enter a number between 8-128.
+
+The user will then confirm whether to include uppercase letters. Select "OK" to include uppercase letters, select "cancel" to not include uppercase letters.
+
+The user will then confirm whether to include lowercase letters. Select "OK" to include lowercase letters, select "cancel" to not include lowercase letters.
+
+The user will then confirm whether to include special characters. Select "OK" to include special characters, select "cancel" to not include special characters.
+
+The user will then confirm whether to include numbers. Select "OK" to include numbers, select "cancel" to not include numbers.
+
+The page should then generate a password.
+
+Please find this project's hosted webpage here: https://gharrison307.github.io/module2challenge/
+
+Please find this project's repository page here: https://github.com/gharrison307/module2challenge
--- script.js
+++ script.js
@@ -1,33 +1,157 @@
 // Assignment code here
 
 // Get references to the #generate element
+
 var generateBtn = document.querySelector("#generate");
 
-var lchars = "abcdefghijklmnopqrstuvwxyz";
-var uchars = "ABCEFGHIJKLMNOPQRSTUVWXYZ";
-var numbers = "1234567890";
-var schars = "!@#$%^&*()_+~{}|[]?";
+// variable declarations
+var numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
-function generatePassword() {
-  var length = prompt(
-    "how many Characters Would you like your password to be? (min: 8 max: 128"
+var lchar = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+var uchars = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+var schars = [
+  "!",
+  "#",
+  "$",
+  "%",
+  "&",
+  ",",
+  "(",
+  ")",
+  "*",
+  "+",
+  "'",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  "<",
+  "=",
+  ">",
+  "?",
+  "@",
+  "[",
+  "]",
+  "^",
+  "_",
+  "'",
+  "{",
+  "}",
+  "|",
+  "~",
+];
+
+function writePassword() {
+  var randomArray = [];
+  var newPassword = " ";
+
+  // Intial prompt
+  var characterLength = prompt(
+    "How many characters would you like your password to be? (min: 8 max: 128)"
   );
-  var upper = confirm("Would you like to include upper case letters?");
-  // var special = confirm(" Would you like to inlcude special characters?");
-  // var numbers = confrim("Would you like to include numbers?");
-  console.log(upper);
-  if (upper === true) {
+
+  if (parseFloat(characterLength) < 8 || parseFloat(characterLength) > 128) {
+    alert("Error : Please use a number between 8 and 128");
+    return writePassword();
+  }
+
+  // uppercase confirm
+  var upper = confirm(
+    "Would you like to include upper case letters? (ok: yes | cancel: no)"
+  );
+  if (upper === true) randomArray = randomArray.concat(uchars);
+
+  // console.log(randomArray);   -randomArray check
+
+  // lowercase confirm
+  var lower = confirm(
+    "Would you like to include lower case letters? (ok: yes | cancel: no)"
+  );
+  if (lower === true) randomArray = randomArray.concat(lchar);
+
+  // console.log(randomArray);    -randomArray check
+
+  // special character confirm
+  var special = confirm(
+    " Would you like to inlcude special characters? (ok: yes | cancel: no)"
+  );
+  if (special === true) randomArray = randomArray.concat(schars);
+
+  // console.log(randomArray);     -randomArray check
+
+  // numeric confirm
+  var numbers = confirm(
+    "Would you like to include numbers? (ok: yes | cancel: no)"
+  );
+  if (numbers === true) randomArray = randomArray.concat(numeric);
+
+  // console.log(randomArray);      -randomArray check
+
+  for (var i = 0; i < parseFloat(characterLength); i++) {
+    var randomNumber = Math.floor(Math.random() * randomArray.length);
+
+    newPassword += randomArray[randomNumber];
+
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = newPassword;
   }
 }
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
